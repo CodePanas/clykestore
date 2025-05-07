38f7452e8fb46c97f5c298be672d6880e26ebc72
Raul Gomez <ralexs04@gmail.com>
2025-05-07 10:03:40 -0500

    feat: move colors to file and add suppor to increase and decrease

--- shopify-theme/assets/customizer.js
+++ shopify-theme/assets/customizer.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const baseImage = document.getElementById("base-product-image");
   const selects = document.querySelectorAll("select");
 
+  const quantityInput = document.getElementById("quantity");
+  const increaseBtn = document.getElementById("increase-qty");
+  const decreaseBtn = document.getElementById("decrease-qty");
+
   // Actualiza la imagen según la talla (simulado por nombre de variante)
   selects.forEach((select) => {
     select.addEventListener("change", () => {
@@ -14,6 +18,18 @@ document.addEventListener("DOMContentLoaded", function () {
       // Aquí podrías cambiar dinámicamente la imagen si estuvieras usando overlays
       // baseImage.src = 'URL basadas en selección';
     });
+  });
+
+  increaseBtn.addEventListener("click", () => {
+    let current = parseInt(quantityInput.value) || 1;
+    quantityInput.value = current + 1
+  })
+
+  decreaseBtn.addEventListener("click", () => {
+    let current = parseInt(quantityInput.value) || 1;
+    if (current > 1) {
+      quantityInput.value = current - 1;
+    }
   });
 
   console.log("Customizer JS cargado.");
